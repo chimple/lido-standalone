4310486f2294e3867a9dea7581b1f7fe71bdf853
nehacr7 <neha@sutara.org>
2025-12-16 10:08:27 +0530

    commonAudioPath

--- src/components/lido-standalone/lido-standalone.tsx
+++ src/components/lido-standalone/lido-standalone.tsx
@@ -59,6 +59,8 @@ export class LidoStandalone {
    */
   @Prop() codeFolderPath?: string;
 
+  @Prop() commonAudioPath?: string;
+
   /** Whether scripts are already injected (remote or fallback). */
   @State() scriptsInjected: boolean = false;
 
@@ -266,7 +268,7 @@ export class LidoStandalone {
      * This ensures the custom elements are defined before usage.
      */
 
-    return <lido-home initial-index={this.initialIndex} canplay={this.canplay} height={this.height} xml-data={this.localXmlData} base-url={this.xmlBaseUrl} code-folder-path={this.codeFolderPath}></lido-home>;
+    return <lido-home  common-audio-path={this.commonAudioPath} initial-index={this.initialIndex} canplay={this.canplay} height={this.height} xml-data={this.localXmlData} base-url={this.xmlBaseUrl} code-folder-path={this.codeFolderPath}></lido-home>;
   }
 
   
